8d0f6c96c917441526d608498cbc398e72c0e3e8
iamchingel <iamsanketray123@gmail.com>
2017-08-21 01:25:57 +0530

    Initial Commit

---
+++ Udacity-VirtualTourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udacity-VirtualTourist
+//
+//  Created by Sanket Ray on 21/08/17.
+//  Copyright © 2017 Sanket Ray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
