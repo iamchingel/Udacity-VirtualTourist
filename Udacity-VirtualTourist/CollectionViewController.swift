5d47e06ecbe07e530692320c4db9c67698b92403
iamchingel <iamsanketray123@gmail.com>
2017-08-21 19:29:43 +0530

    First Commit

---
+++ Udacity-VirtualTourist/CollectionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CollectionViewController.swift
+//  Udacity-VirtualTourist
+//
+//  Created by Sanket Ray on 21/08/17.
+//  Copyright © 2017 Sanket Ray. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
